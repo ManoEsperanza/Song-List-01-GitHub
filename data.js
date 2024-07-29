7bc2938b51c013769ff64f8b151e4e528d47e484
mano <mat_nur82@hotmail.com>
2024-07-29 20:37:02 +0800

    Test add and save

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
-const JSON_BIN_BASE_URL="https://api.jsonbin.io/v3";
-const JSON_BIN_ID = "66a24380acd3cb34a86b1dac";    
+const JSON_BIN_BASE_URL = "https://api.jsonbin.io/v3";
+const JSON_BIN_ID = "66a24380acd3cb34a86b1dac";
 
 async function loadSongs() {
     console.log("data loading")
@@ -9,3 +9,36 @@ async function loadSongs() {
     console.log(response.data.record.songs[0])
     return response.data.record.songs;
 }
+
+let songs = [];
+function addSongs(songs,song, artist, rating) {
+    let songinfo = {
+        "id": Math.floor(Math.random() * 10000 + 1),
+        "song": song,
+        "artist": artist,
+        "rating": rating
+    }
+    // title cannot be empty and author cannot be empty
+    if (song == "song" || artist == "artist") {
+        alert("Please provide a song or an artist");
+        // terminate the function
+        return;
+    }
+
+    songs.push(songinfo);
+}
+
+function editSong(songs, songid, song, artist) {
+    // use a linear search to find the book
+    let songToEdit = null;
+    for (let b of song) {
+      if (b.id === songid) {
+       bookToEdit = b;
+      break;
+      }
+    }
+   
+    songToEdit.song = songs;
+    songToEdit.artist= artist;
+  }
+  
--- script.js
+++ script.js
@@ -3,6 +3,23 @@ document.addEventListener("DOMContentLoaded", async function () {
     songs = await loadSongs()
     renderList();
 
+    let allEditButtons = document.querySelectorAll(".edit");
+    for (let button of allEditButtons) {
+        console.log(button)
+        button.addEventListener("click", function (event) {
+            console.log("edit");
+            // the first parameter for a function handling an event is the event info
+            let clickedButton = event.target;
+            let songid = Number(clickedButton.dataset.songid);
+            let songs = prompt("Enter the new song");
+            let artist = prompt("Enter the new artist");
+            let rating = prompt("Enter the rating")
+            editSong(songs, songid, artist, rating);
+            // renderList();
+
+        });
+    }
+
 })
 
 function renderList() {
@@ -11,12 +28,27 @@ function renderList() {
     console.log(songs);
     for (let b of songs)
         outputString += `<tr> 
-    <td>${b.id }</td><td>${b.song}</d><td>${b.artist}</td><td>${b.rating}</td>
+    <td>${b.id}</td><td>${b.song}</d><td>${b.artist}</td><td>${b.rating}</td>
     <td>
-    <button class="edit" data-bookid="${b.id}">Edit</button>
-    <button class="delete" data-bookid="${b.id}">Delete</button>
+    <button class="edit" data-songid="${b.id}">Edit</button>
+    <button class="delete" data-songid="${b.id}">Delete</button>
     </td>
-    </tr>`;
+    </tr>`
 
     songsListDiv.innerHTML = outputString;
 }
+
+let addBtn = document.querySelector("#addBtn");
+addBtn.addEventListener("click", function () {
+    let songs = document.querySelector("#song").value;
+    let artist = document.querySelector("#artist").value;
+    let rating = document.querySelector("#rating").value;
+    addsong(songs, artist, rating);
+    // renderList();
+});
+
+let saveBtn = document.querySelector("#saveBtn");
+saveBtn.addEventListener("click", function () {
+    saveSongs(songs);
+})
+
